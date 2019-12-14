2eca8aca278b6a21335f4d5886bae6a25a17abcd
lichpoppy <1033968138@qq.com>
2019-12-14 16:50:12 +0800

    谢鹭毅的第三次修改

--- src/main/java/com/ywb/text/TextApplication.java
+++ src/main/java/com/ywb/text/TextApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TextApplication
 {
-
+    private String LL;
 	public static void main(String[] args)
 	{
 		SpringApplication.run(TextApplication.class, args);
